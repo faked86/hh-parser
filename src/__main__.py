5857dc18e0ddbce7e19aad5b5cea53210e3fa16e
Daniil Ulturgashev <ulturgashev@apsolutions.ru>
2023-01-28 05:50:46 +0700

    prettier string

--- src/__main__.py
+++ src/__main__.py
@@ -6,13 +6,12 @@ from src.downloader import download
 if __name__ == "__main__":
     # TODO: custom query builder
     print(
-        """1. Go to hh.ru
-2. Enter vacancy name
-3. Press `Enter`
-4. Change filters
-5. Copy URL
-6. Check count of pages
-"""
+        "1. Go to hh.ru\n"
+        "2. Enter vacancy name\n"
+        "3. Press `Enter`\n"
+        "4. Change filters\n"
+        "5. Copy URL\n"
+        "6. Check count of pages\n"
     )
 
     base_url = input("Enter copied URL: ")
