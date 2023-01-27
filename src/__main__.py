b63ee2276f499634594c2c991f5b7e9ac0567e23
Daniil Ulturgashev <ulturgashev@apsolutions.ru>
2023-01-28 05:36:02 +0700

    url and page count from user

--- src/__main__.py
+++ src/__main__.py
@@ -4,13 +4,23 @@ from src.downloader import download
 
 
 if __name__ == "__main__":
-    htmls = asyncio.run(
-        download(
-            "https://novosibirsk.hh.ru/search/vacancy?only_with_salary=true&text=%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%BD%D1%8B%D0%B9+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA",
-            11,
-        )
+    # TODO: custom query builder
+    print(
+        """1. Go to hh.ru
+2. Enter vacancy name
+3. Press `Enter`
+4. Change filters
+5. Copy URL
+6. Check count of pages
+"""
     )
 
-    for i, html in enumerate(htmls.HTMLs):
-        with open(f"htmls/{i}.html", "w") as f:
-            f.write(html)
+    base_url = input("Enter copied URL: ")
+    page_number = int(input("Enter number of pages: "))
+
+    htmls = asyncio.run(
+        download(
+            base_url,
+            page_number,
+        )
+    )
